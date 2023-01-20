afa884a34676783813fba77f630c2e29e2a5bbd6
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-01-20 21:32:03 +0100

    First win

--- Players/SmartPlayer/src/main/java/put/ai/games/smartplayer/SmartPlayer.java
+++ Players/SmartPlayer/src/main/java/put/ai/games/smartplayer/SmartPlayer.java
@@ -12,13 +12,14 @@ import put.ai.games.game.Player;
 
 public class SmartPlayer extends Player {
 
-    private int maxDepth = 1000;
+    private int maxDepth = 10;
 
     private long startTime;
     private boolean finishTime;
 
     private double timeframe;
     private Move bestMove;
+    private int bestResult;
 
 
     @Override
@@ -48,13 +49,19 @@ public class SmartPlayer extends Player {
     }
 
     private int minmax(Board b, int depth, Color currentPlayer, int alpha, int beta, boolean maximizingPlayer, ArrayList<Move> moves) {
-        if (finishTime) {
-            return b.getMovesFor(Color.PLAYER1).size()-b.getMovesFor(Color.PLAYER2).size();
+        if (maximizingPlayer && b.getWinner(currentPlayer) != Color.EMPTY && b.getMovesFor(Color.PLAYER1).size()-b.getMovesFor(Color.PLAYER2).size() > bestResult) {
+            bestResult = b.getMovesFor(Color.PLAYER1).size()-b.getMovesFor(Color.PLAYER2).size();
+            bestMove = moves.get(0);
         }
+        else if (!maximizingPlayer && b.getWinner(currentPlayer) != Color.EMPTY && b.getMovesFor(Color.PLAYER1).size()-b.getMovesFor(Color.PLAYER2).size() < bestResult){
+
+        }
+
         // jeśli algorytm:
+        // - skończy przewidziany czas
         // - dojdzie do maksymalnej wskazanej głębokości,
         // - dojdzie do celu
-        if (depth == maxDepth || b.getWinner(currentPlayer) == getColor()) {
+        if (finishTime || depth == maxDepth || b.getWinner(currentPlayer) == getColor()) {
             return b.getMovesFor(Color.PLAYER1).size()-b.getMovesFor(Color.PLAYER2).size();
         }
 
@@ -66,9 +73,9 @@ public class SmartPlayer extends Player {
                 ArrayList<Move>nextMoves = (ArrayList<Move>) moves.clone();
                 nextMoves.add(nextMove);
                 if (currentPlayer == Color.PLAYER1){
-                    value = Math.max(value, minmax(nextBoard, depth + 1, Color.PLAYER2, alpha, beta, true, nextMoves));
+                    value = Math.max(value, minmax(nextBoard, depth + 1, Color.PLAYER2, alpha, beta, maximizingPlayer, nextMoves));
                 } else{
-                    value = Math.max(value, minmax(nextBoard, depth + 1, Color.PLAYER1, alpha, beta, true, nextMoves));
+                    value = Math.max(value, minmax(nextBoard, depth + 1, Color.PLAYER1, alpha, beta, maximizingPlayer, nextMoves));
                 }
 
                 alpha = Math.max(alpha, value);
@@ -90,9 +97,9 @@ public class SmartPlayer extends Player {
                 ArrayList<Move>nextMoves = (ArrayList<Move>) moves.clone();
                 nextMoves.add(nextMove);
                 if (currentPlayer == Color.PLAYER1){
-                    value = Math.min(value, minmax(nextBoard, depth + 1, Color.PLAYER2, alpha, beta, false, nextMoves));
+                    value = Math.min(value, minmax(nextBoard, depth + 1, Color.PLAYER2, alpha, beta, maximizingPlayer, nextMoves));
                 } else{
-                    value = Math.min(value, minmax(nextBoard, depth + 1, Color.PLAYER1, alpha, beta, false, nextMoves));
+                    value = Math.min(value, minmax(nextBoard, depth + 1, Color.PLAYER1, alpha, beta, maximizingPlayer, nextMoves));
                 }
 
                 beta = Math.min(beta, value);
@@ -106,62 +113,4 @@ public class SmartPlayer extends Player {
         }
     }
 
-//    private int minmax2(Board b, ArrayList<Move> moves, Color currentPlayer, int depth, int alpha, int beta){
-//        if (finishTime){
-//            return b.getMovesFor(Color.PLAYER1).size()-b.getMovesFor(Color.PLAYER2).size();
-//        }
-//        // jeśli algorytm:
-//        // - dojdzie do maksymalnej wskazanej głębokości,
-//        // - dojdzie do celu
-//        if (depth == maxDepth || b.getWinner(currentPlayer) == getColor()){
-//            if (b.getMovesFor(Color.PLAYER1).size()==0){
-//                return Integer.MAX_VALUE;
-//            }
-//            else if (b.getMovesFor(Color.PLAYER2).size()==0){
-//                return Integer.MIN_VALUE;
-//            }
-//            return b.getMovesFor(Color.PLAYER1).size()-b.getMovesFor(Color.PLAYER2).size();
-//        }
-//
-//        if (currentPlayer == Color.PLAYER1){
-//            int value = Integer.MIN_VALUE;
-//            for (Move nextMove : b.getMovesFor(currentPlayer)){
-//                Board nextBoard = b.clone();
-//                nextBoard.doMove(nextMove);
-//                ArrayList<Move>nextMoves = (ArrayList<Move>) moves.clone();
-//                nextMoves.add(nextMove);
-//                value = Math.max(value, minmax(nextBoard, nextMoves, currentPlayer,
-//                                         depth+1, alpha, beta));
-//                alpha = Math.max(alpha, value);
-//                if (alpha >= beta ||
-//                    System.currentTimeMillis()-startTime >= timeframe){
-//                    if (bestMove == null){
-//                        bestMove = nextMove;
-//                    }
-//                    finishTime = true;
-//                    break;
-//                }
-//            }
-//            return value;
-//        }
-//        else if (currentPlayer == Color.PLAYER2){
-//            int value = Integer.MAX_VALUE;
-//            for (Move nextMove : b.getMovesFor(currentPlayer)){
-//                Board nextBoard = b.clone();
-//                nextBoard.doMove(nextMove);
-//                ArrayList<Move>nextMoves = (ArrayList<Move>) moves.clone();
-//                nextMoves.add(nextMove);
-//                value = Math.min(value, minmax(nextBoard, nextMoves, currentPlayer,
-//                        depth+1, alpha, beta));
-//                alpha = Math.min(alpha, value);
-//                if (alpha >= beta ||
-//                        System.currentTimeMillis()-startTime >= timeframe){
-//                    finishTime = true;
-//                    break;
-//                }
-//            }
-//            return value;
-//        }
-//        return 0; // nigdy tu nie dojdzie
-//    }
 }
